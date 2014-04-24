16b9028d297ab0a538c313ab746824edaa1329d3
Brian Dunnette <brian@dunnette.us>
2014-04-23 19:33:47 -0500

    moving back to django 1.6 for the moment

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,38 +1,116 @@
-# encoding: utf8
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
-from django.db import models, migrations
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Repository'
+        db.create_table(u'djecks_omeka_repository', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+        ))
+        db.send_create_signal(u'djecks_omeka', ['Repository'])
+
+        # Adding model 'Collection'
+        db.create_table(u'djecks_omeka_collection', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('items_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('repository', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djecks_omeka.Repository'])),
+            ('deck', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djecks.Deck'], null=True, blank=True)),
+        ))
+        db.send_create_signal(u'djecks_omeka', ['Collection'])
+
+        # Adding model 'Item'
+        db.create_table(u'djecks_omeka_item', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('files_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('collection', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djecks_omeka.Collection'])),
+            ('case', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djecks.Case'], null=True, blank=True)),
+        ))
+        db.send_create_signal(u'djecks_omeka', ['Item'])
+
+        # Adding model 'Image'
+        db.create_table(u'djecks_omeka_image', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('image_annotated', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djecks_omeka.Item'])),
+        ))
+        db.send_create_signal(u'djecks_omeka', ['Image'])
 
 
-class Migration(migrations.Migration):
+    def backwards(self, orm):
+        # Deleting model 'Repository'
+        db.delete_table(u'djecks_omeka_repository')
 
-    dependencies = [
-        (b'djecks', b'__first__'),
-    ]
+        # Deleting model 'Collection'
+        db.delete_table(u'djecks_omeka_collection')
 
-    operations = [
-        migrations.CreateModel(
-            name=b'Repository',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                (b'url', models.URLField()),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name=b'Collection',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                (b'url', models.URLField()),
-                (b'items_url', models.URLField(blank=True)),
-                (b'title', models.CharField(max_length=255, blank=True)),
-                (b'repository', models.ForeignKey(to=b'djecks_omeka.Repository', to_field='id')),
-                (b'deck', models.ForeignKey(to_field='id', blank=True, to=b'djecks.Deck', null=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-    ]
+        # Deleting model 'Item'
+        db.delete_table(u'djecks_omeka_item')
+
+        # Deleting model 'Image'
+        db.delete_table(u'djecks_omeka_image')
+
+
+    models = {
+        u'djecks.case': {
+            'Meta': {'object_name': 'Case'},
+            'age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'decks': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['djecks.Deck']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sex': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'djecks.deck': {
+            'Meta': {'object_name': 'Deck'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'djecks_omeka.collection': {
+            'Meta': {'object_name': 'Collection'},
+            'deck': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djecks.Deck']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'items_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'repository': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djecks_omeka.Repository']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'djecks_omeka.image': {
+            'Meta': {'object_name': 'Image'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'image_annotated': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djecks_omeka.Item']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'djecks_omeka.item': {
+            'Meta': {'object_name': 'Item'},
+            'case': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djecks.Case']", 'null': 'True', 'blank': 'True'}),
+            'collection': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djecks_omeka.Collection']"}),
+            'files_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'djecks_omeka.repository': {
+            'Meta': {'object_name': 'Repository'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['djecks_omeka']
